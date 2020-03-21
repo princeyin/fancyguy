417375879cfc2885442ee1b9063f7a48689f723f
princeyin <2308961836@qq.com>
2020-03-21 18:38:54 +0800

    Update vue.js

--- vue.js
+++ vue.js
@@ -15,6 +15,23 @@ https://cn.vuejs.org/
          v-once  vue的数据改变不会再影响DOM
          v-html 可以render Vue变量中含有HTML代码  <div v-html="data">
          v-class 动态类名
+         指令的动态参数：v-bind:[attr]  可以动态的绑定属性和属性值 attr的名字不能有大写字母，动态参数表达式有一些语法约束，因为某些字符，如空格和引号
+         变通的办法是使用没有空格或引号的表达式
+         修饰符 . 指明的特殊后缀，用于指出一个指令应该以特殊方式绑定  <form v-on:submit.prevent="onSubmit">...</form> 
+         指令对于触发的事件调用 event.preventDefault()
+         缩写： v-bind:href-- :href  v-bind:[attr]--:[attr] 
+              v-on:click -- @click   v-on:[event]--@[event]
+Vue的计算属性： computed:{} 存放一些需要计算并返回值的函数 或表达式
+   计算属性缓存VS方法：每次render模板，方法都会执行一次函数   
+   只要每渲染一次模板时，函数返回值不会变的情况下，是不会再次执行函数，而是在缓存中获取值
+   方法没有缓存， 计算属性有缓存
+绑定class 和style 
+  绑定多个class的对象法：v-bind:class="{active:true,'hover-n':true..}[classObj]" 类名有短线链接的要加引号 可以用Vue里面的对象直接绑定
+  绑定多个class的数组方法 :class=['active','hover']|classarr
+  绑定style多个属性也一样
+条件渲染
+  v-else 元素必须紧跟在带 v-if 或者 v-else-if 的元素的后面，否则它将不会被识别
+  v-else-if 元素必须紧跟在带 v-if 或者 v-else-if 的元素的后面，否则它将不会被识别  v-else-if 可以重复使用多个
 组件
   // 定义名为 todo-item 的新组件
   Vue.component('todo-item', { template: '<li>这是个待办项</li>'}) 然后在DOM中<todo-item></todo-item> 可以对这个元素添加属性 添加Vue的数据
